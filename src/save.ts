f688bd1c1c70701789818606e224fc34d1406b6a
Tanuj Kumar Mishra <tanuj077@users.noreply.github.com>
2022-11-16 12:51:19 +0000

    remove debug logs

--- src/save.ts
+++ src/save.ts
@@ -11,7 +11,6 @@ process.on("uncaughtException", e => utils.logWarning(e.message));
 
 async function run(): Promise<void> {
     try {
-        core.info(String(core.getInput(Inputs.SkipSave)===null)+"...."+String(core.getInput(Inputs.SkipSave)===''));
         if (!utils.isCacheFeatureAvailable() || core.getInput(Inputs.SkipSave)==='yes') {
             if(core.getInput(Inputs.SkipSave)==='yes'){
                 core.info("Not saving the cache as skip-save is: "+ core.getInput(Inputs.SkipSave));
